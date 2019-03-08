77c935003e31fc98d2588ea9a847f721fb519a17
dr-offig <dr.offig@offig.net>
2019-03-08 03:12:04 +0100

    Spectrogram functional (but not interactive)

--- R/audiofile.R
+++ R/audiofile.R
@@ -6,6 +6,7 @@ library(stats)
 library(signal)
 library(R6)
 library(ggplot2)
+library(scales)
 library(plotly)
 #library(ggthemr)
 #library(shiny)
@@ -137,7 +138,7 @@ Audiofile <- R6::R6Class("Audiofile",
                         return(fft(wx) / length(wx))
                       }
                       fullTransform <- lapply(0:m,gt)
-                      powerSpectrum <- sapply(fullTransform,abs)
+                      powerSpectrum <- sapply(fullTransform,function(z) { abs(z)^2 })
                       halfway <- floor(dim(powerSpectrum)[1] / 2)
                       self$spectrogram <- as.data.table(t(powerSpectrum[1:halfway,]))
                       self$spectrogram[, spectroBlock := .I]
@@ -196,6 +197,117 @@ Audiofile <- R6::R6Class("Audiofile",
 )
 
 
+###### Apply an arbitrary analysis window by window ######
+Audiofile$set("public", "apply", function(analysis=rms, wt=signal::hanning, n=512, h=256, ch=1, offset=0, units="seconds",from=0,
+                                          to=if(units == "seconds") { self$frames / self$samplerate } else { self$frames }) {
+
+  a <- if(units == "seconds") from * self$samplerate else from
+  b <- if(units == "seconds") to * self$samplerate else to
+  A <- floor(a / h)
+  B <- floor((b-(n-1)) / h)
+
+
+  cat(paste0("Analysing audio file ", basename(self$filename), " frames [", a,":", b, "] with window size ",n , " and hop ",h, "\n"))
+  #m <- floor((self$frames - n) / h)
+  #self$audioData[, spectroBlock := (((frame + (h/2) - (n/2)) %/% h)+1)]
+  gt <- function(i) {
+    x <- unlist(self$audioData[(offset + i*h + 1):(offset + i*h + n),ch:ch],use.names=FALSE)
+    wx <- x * wt(n)
+    return(analysis(wx))
+  }
+  fullAnalysis <- lapply(A:B,gt)
+
+})
+
+
+######### utilities ########
+Audiofile$set("public", "around", function(t,w=4096,ch=1)
+{
+  start_sample <- t - floor(w/2)
+  end_sample <- t + floor(w/2) - 1
+  chan <- paste0("channel",toString(ch))
+  snippet <- as.matrix(self$audioData[(start_sample:end_sample),..chan])
+  f <- snippet[,1]
+  return(f)
+})
+
+
+Audiofile$set("public","spectrumBin2Freq",function(bin) {
+
+  freqBinWidth <- self$samplerate / self$spectrogramWindowSize
+  return(freqBinWidth * (bin - 1))
+
+})
+
+
+Audiofile$set("public","freq2spectrumBin",function(freq) {
+  freqBinWidth <- self$samplerate / self$spectrogramWindowSize
+  return(freq/freqBinWidth)
+})
+
+
+Audiofile$set("public","timeBin2StartSample",function(bin) {
+
+  return(self$spectrogramHop * (bin - 1))
+
+})
+
+
+Audiofile$set("public","snippet",
+              function(units=c("samples","seconds","hms","spectral_blocks","cq_blocks","env_blocks"),
+                       from=0,
+                       to=if(units=="samples") { from + 4096 }
+                       else if (units == "seconds") {from + 1.0 }
+                       else if (units == "hms") { as.hms(from + 1) }
+                       else {from + 1 })
+{
+  # start_sample <- t - floor(w/2)
+  # end_sample <- t + floor(w/2) - 1
+  # chan <- paste0("channel",toString(ch))
+  # snippet <- as.matrix(self$audioData[(start_sample:end_sample),..chan])
+  # f <- snippet[,1]
+  # return(f)
+
+  start_sample <- if(units=="samples") { from }
+  else if (units == "seconds") { from * self$samplerate }
+  else if (units == "hms") { as.numeric(from) * self$samplerate }
+  else if (units == "spectral_blocks") { from * self$spectrogramWindowSize }
+  else if (units == "cq_blocks") { from * self$constQWindowSize }
+  else if (units == "env_blocks") { from * self$envelopeWindowSize }
+  else { from }
+
+  end_sample <- if(units=="samples") { to }
+  else if (units == "seconds") { to * self$samplerate }
+  else if (units == "hms") { as.numeric(to) * self$samplerate }
+  else if (units == "spectral_blocks") { to * self$spectrogramWindowSize }
+  else if (units == "cq_blocks") { to * self$constQWindowSize }
+  else if (units == "env_blocks") { to * self$envelopeWindowSize }
+  else { to }
+
+  output <- Audiofile$new()
+  output$channels <- self$channels
+  output$samplerate <- self$samplerate
+  output$audioData <- self$audioData[(start_sample:end_sample), c(self$get_channel_names())]
+  output$audioData[, frame:=.I]
+  output$frames <- dim(output$audioData)[[1]]
+  return(output)
+
+})
+
+
+Audiofile$set("public","downsample", function(d=2,offset=0)
+{
+  output <- Audiofile$new()
+  output$channels <- self$channels
+  output$audioData <- self$audioData[(frame+offset) %% d == 0, c(self$get_channel_names())]
+  output$audioData[, frame:=.I]
+  output$frames <- dim(output$audioData)[[1]]
+  output$samplerate <- self$samplerate / d
+  return(output)
+})
+
+
+############# Plotting functions #############
 Audiofile$set("public", "plot", function(units="seconds",from=0,
                                              to=if(units == "seconds") { 1 } else { self$samplerate })
 {
@@ -276,18 +388,34 @@ Audiofile$set("public", "plot.env", function(units="seconds",from=0,
 })
 
 
-Audiofile$set("public", "plot.spectrum", function(units="seconds",from=0,
-                                         to=if(units == "seconds") { self$frames / self$samplerate } else { self$frames },
-                                         minFreq=self$samplerate/self$spectrogramWindowSize,
-                                         maxFreq=self$samplerate/2
-                                         )
+Audiofile$set("public", "plot.spectrogram",
+              function(n=self$spectrogramWindowSize, h=self$spectrogramHop,
+                       wt=self$spectrogramWindow, ch=1,
+                       units="samples", from=0,
+                       to=if(units=="samples") { self$frames }
+                         else if (units == "seconds") {self$frames / self$samplerate }
+                         else if (units == "hms") { as.hms(self$frames / self$samplerate) }
+                         else { ((self$frames - n) %/% h) + 1 },
+                       minFreq=self$samplerate/n,
+                       maxFreq=self$samplerate/2,
+                       trans="identity", contrast=1)
 {
-  if (!self$spectrogramCalculated)
-    { self$calculateSpectrogram() }
+  if ((!self$spectrogramCalculated) || n!=self$spectrogramWindowSize || h!=self$spectrogramHop)
+    self$calculateSpectrogram(n=n, h=h, wt=wt, ch=ch)
+
+  start_sample <- if(units=="samples") { from }
+  else if (units == "seconds") {from * self$samplerate }
+  else if (units == "hms") { as.numeric(from) * self$samplerate }
+  else { ((h-1) * from) + 1 }
+
+  end_sample <- if(units=="samples") { to }
+  else if (units == "seconds") {to * self$samplerate }
+  else if (units == "hms") { as.numeric(to) * self$samplerate }
+  else { ((h-1) * to) + n }
 
   d <- self$spectrogramHop
-  a <- if(units == "seconds") from * self$samplerate else from
-  b <- if(units == "seconds") to * self$samplerate else to
+  a <- start_sample    # if(units == "seconds") from * self$samplerate else from
+  b <- end_sample      # if(units == "seconds") to * self$samplerate else to
   A <- a / d
   B <- b / d
 
@@ -295,8 +423,9 @@ Audiofile$set("public", "plot.spectrum", function(units="seconds",from=0,
 
   # maximum of screen width points should ever be plotted
   screenDim <- dev.size(units="px");
-  w <- screenDim[[1]]
-  h <- screenDim[[2]]
+  w <- 3480    #screenDim[[1]]
+  h <- 2160   #screenDim[[2]]
+  cat(paste0("Screen size is ", as.character(w), " by ", as.character(h)))
   # y <- array("numeric",dim=c(w,2))
   x <- 1:w
   q <- B - A + 1
@@ -305,26 +434,54 @@ Audiofile$set("public", "plot.spectrum", function(units="seconds",from=0,
   # y <- sapply(x,function(i) { self$audioData[((i-1) * B)+1,] })
   #downsampledData <- self$audioData[frame %% B == 0 & frame >= a & frame <= b]
   downsampledData <- self$spectrogram[spectroBlock %% Q == 0 & spectroBlock >= A & spectroBlock <= B]
-  names(downsampledData) <- c(as.character(1:floor(self$spectrogramWindowSize / 2)),"spectroBlock")
+  #names(downsampledData) <- c(as.character(1:floor(self$spectrogramWindowSize / 2)),"spectroBlock")
   #dd <- melt(downsampledData, id.vars="spectroBlock")
 
   #dd <- t(as.matrix(downsampledData[,-c("spectroBlock")]))
   #assign("dd",melt(as.matrix(downsampledData[,-c("spectroBlock")]),variable.factor=FALSE),.GlobalEnv)
-  minFreqBin <- self$samplerate / minFreq
+  #minFreqBin <- self$samplerate / minFreq
+  downsampledData[,time_bin:=.I]
+
+  #dd <- data.table::melt(as.matrix(downsampledData[,-c("spectroBlock")]),variable.factor=FALSE)
+  dd <- data.table::melt(downsampledData[,-c("spectroBlock")],id.vars=c("time_bin"),variable.factor=FALSE)
+  setnames(dd,c("variable","value"),c("freq_bin","power"))
+  dd[,freq_bin:=as.numeric(freq_bin)]
+  dd[,freq:=self$spectrumBin2Freq(freq_bin)]
+  #dd[,startSample:= self$timeBin2StartSample((time_bin * ..Q))]
+
+  # md <- min(dd$power)
+  # if (md > 0)
+  #   dd$power <-10 * log10(dd$power / md)
+  # else
+  #   dd$power <- 10 * log10((dd$power + 10e-13) / 10e-13)
+
+  #dd[,power:=power*..contrast]
+  # minFreqBin <- self$freq2spectrumBin(minFreq)
+  # maxFreqBin <- self$freq2spectrumBin(maxFreq)
+  # #freqs <- self$spectrumBin2Freq(0:(n-1))
+  ddd <- dd[minFreq <= freq & freq <= maxFreq]
 
 
-  dd <- data.table::melt(as.matrix(downsampledData[,-c("spectroBlock")]),variable.factor=FALSE)
-  names(dd) <- c("time_bin","freq_bin","power")
-  md <- min(dd$power)
-  if (md > 0)
-    dd$power <- 10*log10(dd$power / md)
-  else
-    dd$power <- 10*log10((dd$power + 10e-13) / 10e-13)
+  #gp <- ggplot2::ggplot(ddd,aes(xmin=time_bin-1,xmax=time_bin,ymin=freq_bin*self$samplerate/self$spectrogramWindowSize,ymax=(freq_bin+1)*self$samplerate/self$spectrogramWindowSize)) + geom_rect(aes(fill=power)) + scale_y_log10("Frequency") + ggplot2::theme(plot.background=ggplot2::element_rect(fill="black"))
+  gp <- ggplot2::ggplot(ddd, #aes(xmin=time_bin-1, xmax=time_bin, ymin=freq_bin, ymax=(freq_bin+1))
+                        aes(x=time_bin,y=freq_bin)) +
+        geom_raster(aes(fill=power),interpolate = TRUE,show.legend = FALSE) +  #scale_y_log10("Frequency") +
+        scale_x_continuous(name="Time", sec.axis=sec_axis(~(self$timeBin2StartSample(.)))) +
+        scale_y_continuous(name="Frequency", trans=trans,
+                           sec.axis = sec_axis(~(self$spectrumBin2Freq(.)))) +
+        scale_fill_gradient(low="#000000",high="#FF0000", trans=scales::trans_new("contrast",
+                                                    transform=function(x){x^contrast},
+                                                    inverse=function(x){x^(1/contrast)})) +
+        ggplot2::theme(plot.background=ggplot2::element_rect(fill="black")) +
+        ggplot2::theme(panel.background=ggplot2::element_rect(fill="black")) +
+        ggplot2::theme(panel.grid=ggplot2::element_line(colour="#440000")) +
+        ggplot2::theme(axis.line=ggplot2::element_line(colour="black")) +
+        ggplot2::theme(axis.ticks=ggplot2::element_line(colour="#BB0000")) +
+        ggplot2::theme(axis.text=ggplot2::element_text(colour="#BB0000"))
 
 
-  #gp <- ggplot(dd,aes(xmin=time_bin-1,xmax=time_bin,ymin=freq_bin*self$samplerate/self$spectrogramWindowSize,ymax=(freq_bin+1)*self$samplerate/self$spectrogramWindowSize)) + geom_rect(aes(fill=power)) + scale_x_continuous("Block") + scale_y_continuous("Frequency")
-  gp <- ggplot2::ggplot(dd,aes(xmin=time_bin-1,xmax=time_bin,ymin=freq_bin*self$samplerate/self$spectrogramWindowSize,ymax=(freq_bin+1)*self$samplerate/self$spectrogramWindowSize)) + geom_rect(aes(fill=power)) + scale_y_log10("Frequency") + ggplot2::theme(plot.background=ggplot2::element_rect(fill="black"))
-  #gp <- ggplot(dd,aes(x=time_bin-1,y=freq_bin*self$samplerate/self$spectrogramWindowSize)) + geom_segment(aes(xend=time_bin,yend=(freq_bin+1)*self$samplerate/self$spectrogramWindowSize,color=power)) + scale_y_log10("Frequency")
+
+
   return(gp)
   #pp <- plot_ly(dd,x = ~time_bin, y = ~freq_bin, color = ~power, type="scatter", mode='markers', symbol='square',size=20)
   #return(pp)
@@ -371,117 +528,13 @@ Audiofile$set("public", "plot.constQ", function(units="seconds",from=0,
   else
     dd$power <- 10*log10((dd$power + 10e-12) / 10e-13)
 
+
   # qfreq <- function(bin) { self$constQbins[[bin]] }
   gp <- ggplot2::ggplot(dd,aes(xmin=time_bin-1,xmax=time_bin,ymin=freq_bin-1,ymax=freq_bin)) + geom_rect(aes(fill=power))
   return(gp)
 })
 
 
-Audiofile$set("public", "apply", function(analysis=rms, wt=signal::hanning, n=512, h=256, ch=1, offset=0, units="seconds",from=0,
-                                                to=if(units == "seconds") { self$frames / self$samplerate } else { self$frames }) {
-
-  a <- if(units == "seconds") from * self$samplerate else from
-  b <- if(units == "seconds") to * self$samplerate else to
-  A <- floor(a / h)
-  B <- floor((b-(n-1)) / h)
-
-
-  cat(paste0("Analysing audio file ", basename(self$filename), " frames [", a,":", b, "] with window size ",n , " and hop ",h, "\n"))
-  #m <- floor((self$frames - n) / h)
-  #self$audioData[, spectroBlock := (((frame + (h/2) - (n/2)) %/% h)+1)]
-  gt <- function(i) {
-    x <- unlist(self$audioData[(offset + i*h + 1):(offset + i*h + n),ch:ch],use.names=FALSE)
-    wx <- x * wt(n)
-    return(analysis(wx))
-  }
-  fullAnalysis <- lapply(A:B,gt)
-
-})
-
-
-Audiofile$set("public", "around", function(t,w=4096,ch=1)
-{
-  start_sample <- t - floor(w/2)
-  end_sample <- t + floor(w/2) - 1
-  chan <- paste0("channel",toString(ch))
-  snippet <- as.matrix(self$audioData[(start_sample:end_sample),..chan])
-  f <- snippet[,1]
-  return(f)
-})
-
-
-Audiofile$set("public","spectrumBin2Freq",function(bin) {
-
-  freqBinWidth <- self$samplerate / self$spectrogramWindowSize
-  return(freqBinWidth * (bin - 1))
-
-})
-
-
-Audiofile$set("public","freq2spectrumBin",function(freq) {
-  freqBinWidth <- self$samplerate / self$spectrogramWindowSize
-  return(freq/freqBinWidth)
-})
-
-
-Audiofile$set("public","timeBin2StartSample",function(bin) {
-
-  return(self$spectrogramHop * (bin - 1))
-
-})
-
-
-Audiofile$set("public","snippet",
-              function(units=c("samples","seconds","hms","spectral_blocks","cq_blocks","env_blocks"),
-                       from=0,
-                       to=if(units=="samples") { from + 4096 }
-                          else if (units == "seconds") {from + 1.0 }
-                          else if (units == "hms") { as.hms(from + 1) }
-                          else {from + 1 })
-{
-  # start_sample <- t - floor(w/2)
-  # end_sample <- t + floor(w/2) - 1
-  # chan <- paste0("channel",toString(ch))
-  # snippet <- as.matrix(self$audioData[(start_sample:end_sample),..chan])
-  # f <- snippet[,1]
-  # return(f)
-
-  start_sample <- if(units=="samples") { from }
-                  else if (units == "seconds") { from * self$samplerate }
-                  else if (units == "hms") { as.numeric(from) * self$samplerate }
-                  else if (units == "spectral_blocks") { from * self$spectrogramWindowSize }
-                  else if (units == "cq_blocks") { from * self$constQWindowSize }
-                  else if (units == "env_blocks") { from * self$envelopeWindowSize }
-                  else { from }
-
-  end_sample <- if(units=="samples") { to }
-  else if (units == "seconds") { to * self$samplerate }
-  else if (units == "hms") { as.numeric(to) * self$samplerate }
-  else if (units == "spectral_blocks") { to * self$spectrogramWindowSize }
-  else if (units == "cq_blocks") { to * self$constQWindowSize }
-  else if (units == "env_blocks") { to * self$envelopeWindowSize }
-  else { to }
-
-  output <- Audiofile$new()
-  output$channels <- self$channels
-  output$samplerate <- self$samplerate
-  output$audioData <- self$audioData[(start_sample:end_sample), c(self$get_channel_names())]
-  output$audioData[, frame:=.I]
-  output$frames <- dim(output$audioData)[[1]]
-  return(output)
-
-})
-
-Audiofile$set("public","downsample", function(d=2,offset=0)
-{
-  output <- Audiofile$new()
-  output$channels <- self$channels
-  output$audioData <- self$audioData[(frame+offset) %% d == 0, c(self$get_channel_names())]
-  output$audioData[, frame:=.I]
-  output$frames <- dim(output$audioData)[[1]]
-  output$samplerate <- self$samplerate / d
-  return(output)
-})
 
 
 Audiofile$set("public","powermap", function(n=self$spectrogramWindowSize, h=self$spectrogramHop, w=self$spectrogramWindow)
