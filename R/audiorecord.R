e32d8da8b036d5fb6484d96ef17c2b86577f7d62
dr-offig <dr.offig@offig.net>
2019-05-03 09:56:42 +0200

    --

--- R/audiorecord.R
+++ R/audiorecord.R
@@ -157,12 +157,11 @@ function(from=0,fftSize,fftHop,frameWidth,frameHeight, channel=1)
   #lastCandInd <- candInd
   remainingWindows <- numWindows
   while(remainingWindows > 0 && is.finite(candInd)) {
-    # FIXME: need to unload audio when moving onto next audiofile part
-    #if (lastCandInd != candInd) {
-    #  oldAf <- self$audioFiles[[lastCandInd]]
-    #  oldAf$unloadAudio()
-    #  lastCandInd <- candInd
-    #}
+    if (lastCandInd != candInd) {
+      oldAf <- self$audioFiles[[lastCandInd]]
+      oldAf$unloadAudio()
+      lastCandInd <- candInd
+    }
     af <- self$audiofiles[[candInd]]
     af$calculateSpectrogram(n=fftSize, h=fftHop, ch=channel) # will also loadAudio if needed
     cat(sprintf("Calculating spectrogram for part %d of %d\n", candInd, length(self$audiofiles)))
